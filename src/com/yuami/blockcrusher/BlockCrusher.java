8fa2db70bd9542496a65d6217b6829744273368d
majora2007 <josephmajora@gmail.com>
2012-12-11 11:56:00 -0700

    Packaged the bukkit lib inside the lib directory. Refactored package name to my namespace.

---
+++ src/com/yuami/blockcrusher/BlockCrusher.java
@@ -0,0 +1,77 @@
+package com.yuami.blockcrusher;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+/**
+ * BlockCrusher for Bukkit
+ *
+ * @author Dingmatt
+ */
+public class BlockCrusher extends JavaPlugin
+{
+	public static Logger log = Logger.getLogger("Minecraft");
+	public static String PREFIX;
+	public static FileConfiguration config; //TODO Replace this immediately! Very bad!
+	
+
+	private final BlockCrusherBlockListener blockListener = new BlockCrusherBlockListener(this);
+
+	@Override
+	public void onEnable()
+	{
+		PluginDescriptionFile pdfFile = getDescription();
+		PREFIX = "[" +  pdfFile.getName() + " version: " + pdfFile.getVersion() + "] - ";
+
+		getServer().getPluginManager().registerEvents(this.blockListener, this);
+		
+		
+		config = this.getConfig();
+		
+		if (config == null)
+		{
+			logAdd("config.yml failed to load.");
+		}
+		
+		
+		logAdd("BlockCrusher has been enabled.");
+	}
+
+	@Override
+	public void onDisable()
+	{
+		logAdd("BlockCrusher has been disabled.");
+	}
+
+	public static void logAdd(String msg) {
+		log.info(PREFIX + msg);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+	{
+		logAdd("Test1");
+		if (sender instanceof Player) {
+			logAdd("Test2");
+			String command = cmd.getName().toLowerCase();
+			Player player = (Player)sender;
+			if (command.equals("blockcrusher") || command.equals("bc")) {
+				logAdd("Test3");
+				String param1 = args.length > 0 ? args[0] : "";
+				if (param1.equals("reload") || param1.equals("r")) {
+					BlockCrusherConfig.load();
+					player.sendMessage(PREFIX + "Config Reloaded");
+				} else if (param1.equals("help") || param1.equals("h")) {
+					player.sendMessage("/blockcrusher reload");
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/com/yuami/blockcrusher/BlockCrusherConfig.java
@@ -0,0 +1,82 @@
+package com.yuami.blockcrusher;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class BlockCrusherConfig {
+	public static FileConfiguration config;
+	public static HashMap<String, FileConfiguration> locale = new HashMap<String, FileConfiguration>();
+	public static File DATA_FOLDER;
+
+	public static File createDefaultFiles(File file) {
+		if (!file.exists()) {
+			new File(file.getParent()).mkdirs();
+			InputStream in = BlockCrusher.class.getResourceAsStream(file.getName());
+			if (in != null) {
+				FileOutputStream out = null;
+				try {
+					out = new FileOutputStream(file);
+					byte[] buffer = new byte[4096 * 2];
+					int length = 0;
+					while ((length = in.read(buffer)) > 0) {
+						out.write(buffer, 0, length);
+					}
+					BlockCrusher.logAdd(file.getName() + " created.");
+				} catch(IOException e) {
+					e.printStackTrace();
+					BlockCrusher.logAdd("Error creating default file " + file.getName());
+				} finally {
+					try {
+						if (in != null) {
+							in.close();
+						}
+					} catch(IOException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (out != null) {
+							out.close();
+						}
+					} catch(IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		BlockCrusher.logAdd(file.getName() + " loaded.");
+		return file;
+	}
+
+	public static boolean load() {
+		File file = createDefaultFiles(new File(DATA_FOLDER, "config.yml"));
+		if (file.exists()) {
+			config = BlockCrusher.config;
+			try {
+				config.load(file);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//config.configuration(file);
+			//config.load();
+			return true;
+		} else {
+			BlockCrusher.logAdd("Error loading configuration.");
+			return false;
+		}
+	}
+
+}
