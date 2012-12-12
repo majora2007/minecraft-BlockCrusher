b15d0cd05059189780780f8dff28a79255803abb
majora2007 <josephmajora@gmail.com>
2012-12-12 10:14:55 -0700

    Fletched out CommandResponse to possibly use it better.

--- src/com/github/majora2007/blockcrusher/CommandResponse.java
+++ src/com/github/majora2007/blockcrusher/CommandResponse.java
@@ -3,6 +3,8 @@
  */
 package com.github.majora2007.blockcrusher;
 
+import org.bukkit.command.CommandSender;
+
 /**
  * @author jvmilazz
  *
@@ -10,13 +12,21 @@ package com.github.majora2007.blockcrusher;
 public class CommandResponse
 {
 	private String responseMessage;
+	private CommandSender respondant;
+	
 	
 	
 	public CommandResponse() {
-		
+		responseMessage = "";
+		respondant = null;
 	}
 	
 	public CommandResponse(String message) {
+		this.responseMessage = message;
+	}
+	
+	public CommandResponse(CommandSender respondant, String message) {
+		this.respondant = respondant;
 		this.responseMessage = message;
 	}
 	
@@ -26,6 +36,14 @@ public class CommandResponse
 	
 	public String getResponseMessage() {
 		return this.responseMessage;
+	}
+	
+	public void setRespondant(CommandSender respondant) {
+		this.respondant = respondant;
+	}
+	
+	public CommandSender getRespondant() {
+		return this.respondant;
 	}
 	
 }
