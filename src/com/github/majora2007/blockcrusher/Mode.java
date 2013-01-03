e86bdf0e30625a70db0191625ce5ff7a00a861f2
majora2007 <josephmajora@gmail.com>
2013-01-03 11:24:28 -0700

    Added copyright info.

--- src/com/github/majora2007/blockcrusher/Mode.java
+++ src/com/github/majora2007/blockcrusher/Mode.java
@@ -1,3 +1,7 @@
+/**
+ * Distributed under the The Non-Profit Open Software License version 3.0 (NPOSL-3.0)
+ * http://www.opensource.org/licenses/NOSL3.0
+ */
 package com.github.majora2007.blockcrusher;
 
 public enum Mode
